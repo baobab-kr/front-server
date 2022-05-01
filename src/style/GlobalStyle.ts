9b88732a41fdc9d5bb84e67f0a440b5ba71eef1a
GwonM <hideb36577@naver.com>
2022-05-01 23:13:45 +0900

    feat: 전역에서 사용하는 Style 추가

--- src/style/GlobalStyle.ts
+++ src/style/GlobalStyle.ts
@@ -10,7 +10,22 @@ const GlobalStyle = createGlobalStyle`
 
   body {
     line-height: 1;
+    color: black;
   }
+
+  /* input 기본 스타일 초기화 */
+  input {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+  }
+  select {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+  }
+
+
 `;
 
 export default GlobalStyle;
