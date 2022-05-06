4c2b945b6a194e3496b22e01dcb4443a9da9bbdd
GwonM <hideb36577@naver.com>
2022-05-06 11:25:35 +0900

    update: 개인페이지 Router 업데이트

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import MainPage from "./pages/main/MainPage";
+import PersonPage from "./pages/person/PersonPage";
+
 import GlobalStyle from "./style/GlobalStyle";
 export default function App(): JSX.Element {
   return (
@@ -13,6 +15,8 @@ export default function App(): JSX.Element {
           <Header />
           <Routes>
             <Route path="/" element={<MainPage />} />
+            <Route path="/@:id" element={<PersonPage />} />
+            <Route path="*" element={<div>Not Found</div>} />
           </Routes>
         </BrowserRouter>
       </div>
