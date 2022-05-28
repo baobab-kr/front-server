29beeca2c182f5f730003ce79f202dabd99485cc
장한솔 <hansoljang148@gmail.com>
2022-05-28 11:45:40 +0900

    update: 답글 실시간 업데이트 이슈 남음

--- src/pages/index/indexPage.tsx
+++ src/pages/index/indexPage.tsx
@@ -7,16 +7,14 @@ import { useRecoilState } from "recoil";
 import { USER } from "../../store/store.user";
 import { useLocation } from "react-router-dom";
 import { createComment, createReComment, getBoardDetail, getComments, getReComments, patchDeleteComment, patchDeleteReComment } from "../../api/indexPage";
+import moment from "moment";
+import { getReComment } from "@src/Types/indexPage";
 
 export default function IndexPage() {
   const location = useLocation();
+  const board_witer = location.pathname.split("@")[1];
   const board_id = location.pathname.split("@")[2];
   const [userInfo] = useRecoilState(USER);
-  // const test = [
-  //   { re_nicname: "장한솔2", re_date: "2022-02-02", recomment_description: "대댓글 내용 1" },
-  //   { re_nicname: "답글단사람2", re_date: "2022-02-03", recomment_description: "대댓글 내용 2" },
-  // ];
-
   const [indexPageData, setIndexPageData] = useState({ title: "", content: "", description: "", date: "" });
   const [indexPageTag, setIndexPageTag] = useState({ tag: [] });
   const [commentArray, setCommentArray] = useState<any>({ comments: [] });
@@ -32,9 +30,6 @@ export default function IndexPage() {
     setComment(e.target.value);
   };
 
-  const handleReCommentChange = (e: any, idx: number) => {
-    setReComment(e.target.value);
-  };
   const onSaveComment = () => {
     createComment(comment, parseInt(board_id)).then(() => {
       setComment("");
@@ -48,19 +43,35 @@ export default function IndexPage() {
     });
   };
 
-  const onSaveReComment = (comment_id: number, content: string) => {
+  const onSaveReComment = (comment_id: number, content: string, idx: number) => {
     console.log(comment_id, content);
-    createReComment(content, comment_id).then(() => {
-      console.log("답글추가됨");
-      reCommentLoad(comment_id);
-    });
+
+    createReComment(content, comment_id)
+      .then(() => {
+        console.log("답글추가됨");
+        document
+          .querySelector(`.comment_${idx}`)
+          ?.querySelectorAll(".recomment")!
+          .forEach((data) => {
+            console.log(data);
+            data.remove();
+          });
+        setTimeout(() => {
+          reCommentLoad(comment_id, idx);
+        }, 100);
+      })
+      .catch((err) => {
+        console.log("오류남");
+      });
   };
   useEffect(() => {
     const apiGet = async () => {
       await getBoardDetail(parseInt(board_id))
         .then((res) => {
+          console.log(res);
           getComments(parseInt(board_id))
             .then((res) => {
+              console.log(res);
               setCommentArray({ comments: res });
             })
             .catch((err) => {
@@ -77,7 +88,7 @@ export default function IndexPage() {
     apiGet();
   }, []);
 
-  const writeRecomment = (idx: number) => {
+  const writeRecomment = (id: number, idx: number) => {
     const recommentInput = document.createElement("div");
     recommentInput.className = "writeRecommentBox";
     const line = document.createElement("div");
@@ -96,85 +107,87 @@ export default function IndexPage() {
 
     recommentInput.appendChild(input);
 
-    // const inputs = document.querySelectorAll("input");
-
-    // inputs.addEventListener("change", function (e) {
-    //   handleReCommentChange(e, idx);
-    // });
-
     const btn = document.createElement("button");
     btn.className = "saveComment";
     btn.textContent = "답글 작성";
 
     btn.addEventListener("click", () => {
-      onSaveReComment(idx, document.querySelectorAll("input")[idx + 2].value);
+      const inputtest = document.querySelector("#commentBox")!;
+      console.log(inputtest.querySelectorAll("input"));
+      const reCommentText = inputtest.querySelectorAll("input")[idx].value;
+      inputtest.querySelectorAll("input")[idx].value = "";
+      onSaveReComment(id, reCommentText, idx);
     });
     recommentInput.appendChild(btn);
     return recommentInput;
   };
 
-  const reCommentLoad = (idx: number) => {
-    getReComments(idx)
-      .then((res) => {
-        console.log("----sadasdasd----------------");
-        console.log(res);
-        const reCommentObj: any = { comments: res };
-        setReCommentArray({ comments: res });
-        console.log(reCommentArray);
-        reCommentObj.comments.map((data: any) => {
-          console.log("--------------------");
-          console.log(data);
-          const recomment = document.createElement("div");
-          recomment.className = "recomment";
+  const reCommentLoad = (id: number, idx: number) => {
+    setTimeout(() => {
+      getReComments(id)
+        .then((res) => {
+          const reCommentObj: any = { comments: res };
+          console.log(reCommentObj);
+          reCommentObj.comments.map((data: any) => {
+            const recomment = document.createElement("div");
+            recomment.className = "recomment";
 
-          const nickname = document.createElement("div");
-          nickname.className = "re_nickname";
-          nickname.textContent = data.writer.username;
+            const nickname = document.createElement("div");
+            nickname.className = "re_nickname";
+            nickname.textContent = data.writer.username;
 
-          const date = document.createElement("div");
-          date.className = "re_date";
-          date.textContent = data.date;
+            const date = document.createElement("div");
+            date.className = "re_date";
+            date.textContent = moment(data.date).format("YYYY년 MM월 DD일");
 
-          const recomment_description = document.createElement("div");
-          recomment_description.className = "recomment_description";
-          recomment_description.textContent = data.content;
-          if (data.writer.username === userInfo.username) {
-            const recomment_delete = document.createElement("button");
-            recomment_delete.className = "deleteReComment";
-            recomment_delete.textContent = "삭제";
-            recomment.appendChild(recomment_delete);
+            const recomment_description = document.createElement("div");
+            recomment_description.className = "recomment_description";
+            recomment_description.textContent = data.content;
+            if (data.writer.username === userInfo.username) {
+              const recomment_delete = document.createElement("button");
+              recomment_delete.className = "deleteReComment";
+              recomment_delete.textContent = "삭제";
+              recomment.appendChild(recomment_delete);
 
-            recomment_delete.addEventListener("click", () => {
-              deleteReComment(data.id, idx);
-            });
-          }
-          const comment = document.querySelector(`.comment_${idx} .recommentBox`)!;
-          comment.appendChild(recomment);
+              recomment_delete.addEventListener("click", () => {
+                deleteReComment(data.id, idx);
+              });
+            }
+            const comment = document.querySelector(`.comment_${idx} .recommentBox`)!;
+            comment.appendChild(recomment);
 
-          recomment.appendChild(nickname);
-          recomment.appendChild(date);
-          recomment.appendChild(recomment_description);
+            recomment.appendChild(nickname);
+            recomment.appendChild(date);
+            recomment.appendChild(recomment_description);
+
+            setTimeout(() => {
+              document.querySelector(`.input_${idx}`)?.parentElement?.remove();
+              const recommentInput = writeRecomment(id, idx);
+              comment.appendChild(recommentInput);
+            }, 10);
+            return;
+          });
+        })
+        .catch((err) => {
+          console.log(err);
         });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    }, 100);
   };
 
-  const recommentView = (idx: number) => {
-    setIsViewId(idx);
+  const recommentView = async (id: number, idx: number) => {
     const comment = document.querySelector(`.comment_${idx} .recommentBox`)!;
     if (comment?.childElementCount >= 1) {
       comment.innerHTML = "";
     } else {
-      reCommentLoad(idx);
-
-      const recommentInput = writeRecomment(idx);
-      comment.appendChild(recommentInput);
+      reCommentLoad(id, idx);
+      setTimeout(() => {
+        const recommentInput = writeRecomment(id, idx);
+        comment.appendChild(recommentInput);
+      }, 10);
     }
   };
 
-  const deleteComment = (id: number) => {
+  const deleteComment = (id: number, idx: number) => {
     patchDeleteComment(id)
       .then(() => {
         console.log("삭제됨");
@@ -194,8 +207,9 @@ export default function IndexPage() {
     patchDeleteReComment(id)
       .then(() => {
         console.log("삭제됨");
-
-        reCommentLoad(idx);
+        setTimeout(() => {
+          reCommentLoad(id, idx);
+        }, 100);
       })
       .catch((err) => {
         console.log(err, "오류남");
@@ -208,11 +222,11 @@ export default function IndexPage() {
         <Top>
           <div className="title">{indexPageData.title}</div>
           <div className="tag">
-            {indexPageTag.tag.map((data: any) => {
-              return <div>{data.tag_name}</div>;
+            {indexPageTag.tag.map((data: any, idx: number) => {
+              return <div key={idx}>{data.tag_name}</div>;
             })}
           </div>
-          <div className="writer">{indexPageData.description}</div>
+          <div className="writer">{decodeURI(board_witer).slice(0, -1)}</div>
           <div className="description">{indexPageData.description}</div>
           <div className="line"></div>
         </Top>
@@ -229,19 +243,19 @@ export default function IndexPage() {
             댓글 작성
           </button>
         </InputComment>
-        <CommentBox>
+        <CommentBox id="commentBox">
           {commentArray.comments.map((data: any, idx: number) => {
             return (
               <>
-                <div className={`comment comment_${idx}`}>
+                <div className={`comment comment_${idx}`} key={idx}>
                   <div className="nickname">{data.writer.username}</div>
-                  <div className="date">{data.date}</div>
+                  <div className="date">{moment(data.date).format("YYYY년 MM월 DD일")}</div>
                   <div className="comment_description">{data.content}</div>
-                  <button className="recommentView" onClick={() => recommentView(idx)}>
+                  <button className="recommentView" onClick={() => recommentView(data.id, idx)}>
                     댓글 보기
                   </button>
                   {data.writer.username === userInfo.username ? (
-                    <button className="deleteComment" onClick={() => deleteComment(data.id)}>
+                    <button className="deleteComment" onClick={() => deleteComment(data.id, idx)}>
                       삭제
                     </button>
                   ) : null}
