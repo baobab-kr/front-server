b26973ae162f6738db0c06578e732eb4a048e263
GwonM <hideb36577@naver.com>
2022-11-10 02:17:57 +0900

    feat, init 채용페이지 -  일반 사용자 및 서비스 관리자

---
+++ src/pages/jobs/JobManagement/Admin/AdminMgmt.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function AdminMgmt(): JSX.Element {
+  return (
+    <div>
+      <h1>AdminMgmt</h1>
+    </div>
+  );
+}
---
+++ src/pages/jobs/JobManagement/User/UserMgmt.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function UserMgmt(): JSX.Element {
+  return (
+    <div>
+      <h1>UserMgmt</h1>
+    </div>
+  );
+}
